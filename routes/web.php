4459a0325c47aed0570f74b51d4ba784abb7dd64
= <=>
2021-12-25 04:38:45 -0400

    update controller combo and category

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Route::get('/','HomeController@index')->name('home.index');
+});
+
+Route::group(['prefix'=> 'category'], function(){
+    Route::get('/','Manage\CategoryController@index')->name('manage.category.index');
+        Route::post('/','Manage\CategoryController@store')->name('manage.category.store');
+        Route::get('/create','Manage\CategoryController@create')->name('manage.category.create');
+        Route::get('/{Category}/edit','Manage\CategoryController@edit')->name('manage.category.edit');
+        Route::put('/{Category}','Manage\CategoryController@update')->name('manage.category.update');
+        Route::delete('/{Category}','Manage\CategoryController@destroy')->name('manage.category.destroy');
+        Route::get('/{Category}','Manage\CategoryController@show')->name('manage.category.show');
+});
+
+Route::group(['prefix'=> 'combo'], function(){
+    Route::get('/','Manage\ComboController@index')->name('manage.combo.index');
+        Route::post('/','Manage\ComboController@store')->name('manage.combo.store');
+        Route::get('/create','Manage\ComboController@create')->name('manage.combo.create');
+        Route::get('/{Category}/edit','Manage\ComboController@edit')->name('manage.combo.edit');
+        Route::put('/{Category}','Manage\ComboController@update')->name('manage.combo.update');
+        Route::delete('/{Category}','Manage\ComboController@destroy')->name('manage.combo.destroy');
+        Route::get('/{Category}','Manage\ComboController@show')->name('manage.combo.show');
 });
