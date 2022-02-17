62451993fc35e6079c832169f45af2e98a052c31
= <=>
2022-02-17 06:34:11 -0400

    fix name folder category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\internet\InternetRepositoryInterface;
 use App\Repositories\Banner\BannerRepositoryInterface;
-use App\Repositories\category\CategoryRepositoryInterface;
+use App\Repositories\Category\CategoryRepositoryInterface;
 use App\Repositories\Camera\CameraRepositoryInterface;
 use App\Repositories\Posts\PostsRepositoryInterface;
 
--- app/Http/Controllers/ServiceInternetController.php
+++ app/Http/Controllers/ServiceInternetController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\internet\InternetRepositoryInterface;
-use App\Repositories\category\CategoryRepositoryInterface;
+use App\Repositories\Category\CategoryRepositoryInterface;
 class ServiceInternetController extends Controller
 {
 
