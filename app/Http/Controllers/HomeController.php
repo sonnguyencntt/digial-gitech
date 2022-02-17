931c98588078a9aff49f1581c6c74863a498f557
= <=>
2022-02-17 06:42:13 -0400

    fix name folder play and internet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\internet\InternetRepositoryInterface;
+use App\Repositories\Internet\InternetRepositoryInterface;
 use App\Repositories\Banner\BannerRepositoryInterface;
 use App\Repositories\Category\CategoryRepositoryInterface;
 use App\Repositories\Camera\CameraRepositoryInterface;
--- app/Http/Controllers/ServiceInternetController.php
+++ app/Http/Controllers/ServiceInternetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\internet\InternetRepositoryInterface;
+use App\Repositories\Internet\InternetRepositoryInterface;
 use App\Repositories\Category\CategoryRepositoryInterface;
 class ServiceInternetController extends Controller
 {
