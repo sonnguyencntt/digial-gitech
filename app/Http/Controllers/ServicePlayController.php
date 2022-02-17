2e7214d3494667a93eeaa5142f6e1100775ed365
= <=>
2022-02-17 06:10:44 -0400

    fix name category

--- app/Http/Controllers/ServicePlayController.php
+++ app/Http/Controllers/ServicePlayController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\category\CategoryRepositoryInterface;
+use App\Repositories\Category\CategoryRepositoryInterface;
 use App\Repositories\play\PlayRepositoryInterface;
 class ServicePlayController extends Controller
 {
