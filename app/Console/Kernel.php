baa27a6a302069cf0e20364c6adf5b08e44bee03
= <=>
2022-03-10 04:30:05 -0400

    test schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         'App\Console\Commands\ChangeStatusOrderCommand',
-        // 'App\Console\Commands\UpdateSiteMap'
+        'App\Console\Commands\UpdateSiteMap'
     ];
 
     /**
