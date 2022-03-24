18d470520ca9cd2d0c9e6767a3c0dfee55305479
= <=>
2022-03-24 13:17:09 -0300

    log queue forget

--- app/Http/Controllers/Admin/Auth/ForgetPasswordController.php
+++ app/Http/Controllers/Admin/Auth/ForgetPasswordController.php
@@ -48,7 +48,7 @@ class ForgetPasswordController extends Controller
             SendEmailVerifyResetPassword::dispatch($admin);
             return \redirect()->back()->with(["message" => "Đã gửi", "status_code" => "success"]);
         } catch (\Throwable $th) {
-            \dd($th);
+            \Log::channel("jobs")->info($th);
             return \redirect()->back()->with(["message" => "Đã xãy ra lỗi", "status_code" => "danger"]);
         }
     }
